8c388d5e30c59ff1d2e5479917fb1ede6ad71100
Marcela Ortiz <Marcela Ortiz>
2016-04-17 01:26:59 -0500

    Se adiciona la consulta para filtrar los ep por los bounds del map

--- application/modules/geo/models/Ep_model.php
+++ application/modules/geo/models/Ep_model.php
@@ -16,15 +16,36 @@ class Ep_model extends CI_Model {
     * @param Float $lon Longitude
     * @param Int $radius radius to query, the measure is meter
     */
-    function get_entries($lon=null, $lat=null, $radius=500)//
+
+    //FUNCION PARA LLAMAR EP POR ZOOM
+    function get_entries($lon=null, $lat=null, $radius=500)
     {
+        
         $query = $this
                 ->db
-                ->select('id_ep AS id, ST_AsGeoJSON(the_geom) AS geom, fuente, nombre, categoria ', FALSE)
+                ->select('id_ep AS id, ST_AsGeoJSON(the_geom) AS geom, fuente, nombre, categoria, shape_area, barrio, comuna, fechacreacion, fechaactualizacion, idtipo, escala ', FALSE)
                 ->where("ST_DWithin( ST_Transform(ST_GeomFromText('POINT(".$lon." ".$lat.")',  4326), 26986) ,ST_Transform(ep.the_geom, 26986), ".$radius.") ", NULL, FALSE)
-                ->get('ep');
+                ->get('ep');      
 
         return $query->result();
     }
+
+    //FUNCION PARA LLAMAR EP POR LIMITES DE BARRIO
+    function get_entries_by_bounds($bounds)
+    {
+        
+
+        $query = $this
+                ->db
+                ->select ('id_ep as id, ST_AsGeoJSON(the_geom) as geom, categoria, escala, fuente, nombre, shape_area, barrio, comuna, estra_moda as estrato, idtipo, fechaactualizacion, fechacreacion ', FALSE)
+                ->where ("ST_Intersects(ST_PolygonFromText('POLYGON ((".$bounds['southwest']['lng']." ".$bounds['southwest']['lat'].", ".$bounds['northeast']['lng']." ".$bounds['northeast']['lat'].", 
+                ".$bounds['northwest']['lng']." ".$bounds['northwest']['lat'].", ".$bounds['southeast']['lng']." ".$bounds['southeast']['lat'].", ".$bounds['southwest']['lng']." ".$bounds['southwest']['lat']."))', 4326), ep.the_geom)",NULL, FALSE)
+                ->get('ep');      
+
+                 
+
+        return $query->result();
+    }
+
 
 }
